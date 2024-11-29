d3f5158fa47b593c85111bad3f4e9b634503db10
manuellacoelhooo <nucoelho0@gmail.com>
2024-11-28 22:32:17 -0300

    Update style.css

---
+++ style.css
@@ -0,0 +1,33 @@
+body {
+    background-color: #be95bd;
+    color: white;
+    font-family: "Sevillana", cursive;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 700px;
+    margin: 0;
+}
+
+button {
+    background-color: #fffafee9;
+    color: white;
+    font-family: "Sevillana", cursive;
+}
+
+.passo {
+    display: none;
+}
+
+.passo.ativo {
+    display:block;
+}
+
+main {
+    text-align: center;
+    max-width: 90%;
+}
+
+img {
+    max-width: 90%;
+}
