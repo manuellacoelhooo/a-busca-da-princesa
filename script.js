c0190a131ef41d03a424411798c01e9cc6713663
manuellacoelhooo <nucoelho0@gmail.com>
2024-11-28 22:33:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,11 @@
-body {
-    background-color: #be95bd;
-    color: white;
-    font-family: "Sevillana", cursive;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 700px;
-    margin: 0;
-}
+const avanca = document.querySelectorAll('.btn-proximo');
 
-button {
-    background-color: #fffafee9;
-    color: white;
-    font-family: "Sevillana", cursive;
-}
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
-.passo {
-    display: none;
-}
-
-.passo.ativo {
-    display:block;
-}
-
-main {
-    text-align: center;
-    max-width: 90%;
-}
-
-img {
-    max-width: 90%;
-}
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
